88eeff8784892a084c19a4cfdb5162efe64c9a64
Karsten Koerner <kgk1999@gmail.com>
2022-12-08 00:17:41 -0500

    Styled logo component

--- src/components/CLogo.js
+++ src/components/CLogo.js
@@ -7,13 +7,13 @@ export default function CLogo(props) {
     
     if (props.href == null) {
         return (
-            <div className="customponents-logo">
+            <div className="customponents-logo-3xl">
                 <img className="customponents-logo-img" src={props.src} alt={props.alt} />
             </div>
         )
     } else {
         return (
-            <div className="customponents-logo">
+            <div className="customponents-logo-3xl">
                 <a className="customponents-logo-link" href={props.href}>
                     <img className="customponents-logo-img" src={props.src} alt={props.alt} />
                 </a>
